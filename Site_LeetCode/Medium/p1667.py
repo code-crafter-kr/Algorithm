4e4217c825fb7c87b7de5a4c396a73709590c450
unknown <sej324@lehigh.edu>
2024-01-14 11:30:22 -0500

    solved p1704 Leetcode, solving p1667 Leetcode

---
+++ Site_LeetCode/Medium/p1667.py
@@ -0,0 +1,18 @@
+def closeStrings( word1, word2):
+    lst_word1 = [0] * 26
+    lst_word2 = [0] * 26
+
+    if len(word1) != len(word2):
+        return False
+    else:
+        # logic to explore and check the conditions
+        for i in range(len(word1)):
+            lst_word1[ord(word1[i]) - ord('a')] += 1
+            lst_word2[ord(word2[i]) - ord('a')] += 1
+
+    lst_word1.sort()
+    lst_word2.sort()
+    return lst_word1 == lst_word2
+
+
+closeStrings("abc", "bca")
